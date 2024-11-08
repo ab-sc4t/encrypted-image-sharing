b1a2def4493cb2dc5089992e511e5b306f2cb7f4
ab-sc4t <ab.ayush2612@gmail.com>
2024-11-08 23:26:30 +0530

    Hyperlink on name added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Link } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
 const Footer = () => {
     const theme = useTheme();
     return(
         <Box sx={{textAlign: "center", backgroundColor: theme.palette.background.default, height: "4vh"}}>
-            <Typography variant="h7" sx={{color: theme.palette.text.primary}}>By Ayush Bansal</Typography>
+            <Typography variant="h7" sx={{color: "white"}}>By <Link href="https://github.com/ab-sc4t" sx={{color: theme.palette.text.primary}}>Ayush Bansal</Link></Typography>
         </Box>
     )
 }
